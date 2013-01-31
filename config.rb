ace26fa522c6419997671396e54780e7c17c53d8
Brian West <brian@cubistcastle.com>
2013-01-31 10:20:22 -0600

    change input mask on US is not selected

--- config.rb
+++ config.rb
@@ -24,4 +24,4 @@ javascripts_dir = "javascripts"
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
-# sass_options = { :debug_info => true }
+sass_options = { :debug_info => true }
