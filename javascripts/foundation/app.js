6adfdad540e0ebd3b2fbd0ce9d9fe409ffc5ec59
Brian West <brian@cubistcastle.com>
2013-08-15 11:33:21 -0500

    change form markup to wufoo markup

--- javascripts/foundation/app.js
+++ javascripts/foundation/app.js
@@ -67,22 +67,22 @@ if ( !Modernizr.touch) {
         });*/
      $(".influencer-form").validate({
          rules: {
-          "entry.0.single": "required", // firstname
-          "entry.1.single": "required", // lastname
-          "entry.2.single": {
+          "Field1": "required", // firstname
+          "Field2": "required", // lastname
+          "Field4": {
             required: true,
             email: true
           },
-          "entry.5.single": "required", // country
-          "entry.8.single": "required", // address1
-          "entry.12.single": "required", // city
-          "entry.14.single": "required", // state/prov
-          "entry.18.single": "required", // zip
-          "entry.16.single": "required" // phone
+          "Field13": "required", // country
+          "Field14": "required", // address1
+          "Field20": "required", // city
+          "Field22": "required", // state/prov
+          "Field16": "required", // zip
+          "Field18": "required" // phone
         },
         messages: {
           // name: "Please specify your name",
-          "entry.2.single": {
+          "Field4": {
             required: "We need your email address to contact you",
             email: "That's no email!"
           }
@@ -90,7 +90,7 @@ if ( !Modernizr.touch) {
      });
 
 
-  $('#submit').on('click', function(e) {
+/*  $('#submit').on('click', function(e) {
       if ($(".influencer-form").valid()) {
 
 
@@ -106,7 +106,45 @@ if ( !Modernizr.touch) {
           // $('form').hide();
           // $('.thanks').show().addClass('animated bounceInLeft');
       }
-  });
+  });*/
+// $("form").on("submit", function(event) {
+//   event.preventDefault();
+//  alert('got it');
+//   var form = $(this);
+//   var submit = form.find("input[type='submit']");
+//   var oldVal = submit.val();
+ 
+//   submit.prop('disabled', true);
+//   submit.val('submitting...');
+ 
+//   function done() {
+//     submit.prop('disabled', false);
+//     submit.val(oldVal);
+//   }
+ 
+//   $.ajax({
+//     url: form.attr("action"),
+//     type: form.attr("method"),
+//     data: form.serialize()
+//   }).then(function() {
+//     done();
+//   }, function() {
+//     done();
+//   });
+// });
+// $('form').on('submit', function(e) {
 
+        // console.log( $("form").serialize() );
+        // var postVar = $("form").serialize();
+        //  $.ajax({
+        //     url: "http://xspreadsheets.google.com/spreadsheet/formResponse",
+        //     data: postVar,
+        //     type: 'POST',
+        //     dataType: "jsonp"
+        // });
+        // $('.main-content').fadeOut('fast');
+        // $('.thanks').fadeIn('slow');
+//     }
+// });
 
 })(jQuery, this);
